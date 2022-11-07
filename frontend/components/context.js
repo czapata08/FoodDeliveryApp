26e63c0592dfb55c910e7e8b431e245db62e1485
Carlos Zapata <carloszapata@Carloss-MBP.cable.rcn.com>
2022-11-07 13:25:47 -0600

    first commit

---
+++ frontend/components/context.js
@@ -0,0 +1,13 @@
+/* /context/AppContext.js */
+
+import React from "react";
+// create auth context with default value
+
+// set backup default for isAuthenticated if none is provided in Provider
+const AppContext = React.createContext({
+  cart: { items: [], total: 0 },
+  user: null,
+  isAuthenticated: false,
+  setAuthenticated: () => {},
+});
+export default AppContext;
