7c7d56ae0d49ccca8a467e730a608ce162925353
Carlos Zapata <carloszapata@Carloss-MacBook-Pro.local>
2023-01-09 17:10:40 -0600

    fixed

---
+++ .history/root/frontend/components/test_20230109162920.js
@@ -0,0 +1,85 @@
+//Operational
+import React, { useContext, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+//Styling
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+
+//Context
+import { useAuth } from "../lib/auth2";
+import AppContext from "./context";
+
+const Test = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  //   const title = "Nurture Kitchen";
+  const { cart } = useContext(AppContext);
+  var { user, setUser, logout } = useAuth();
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      {/* <Head>
+        <title>{title}</title>
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width'
+        />
+      </Head> */}
+      <header>
+        <Navbar className='navbar-expand-lg'>
+          <NavbarBrand href='/'>Nurture Kitchen</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse
+            isOpen={isOpen}
+            navbar>
+            <Nav
+              className='me-auto'
+              navbar>
+              <NavItem>
+                <NavLink href='/display'></NavLink>
+                <NavItem>
+                  {user ? (
+                    <h5>{user.username}</h5>
+                  ) : (
+                    <NavLink href='/register'>
+                      <a className='nav-Navlink'> Sign up</a>
+                    </NavLink>
+                  )}
+                </NavItem>
+                <NavItem>
+                  <NavLink href='/profile'>Support</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href='/login'>Support</NavLink>
+                </NavItem>
+                //show Cart as a tab. set boolean to toggle cart display in it
+                <NavItem>
+                  <NavLink href='/cart'>Support</NavLink>
+                </NavItem>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </header>
+      {/* <Container>{props.children}</Container> */}
+    </div>
+  );
+};
+
+export default Test;
---
+++ .history/root/frontend/pages/index_20230108203008.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from "react";
+import Cart from "../components/cart";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import RestaurantList from "./restaurants/restaurantList";
+import {
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Row,
+  Col,
+  Container,
+} from "reactstrap";
+import AppContext from "../components/context";
+import client from "./client";
+
+function Home() {
+  const addItem = useContext(AppContext);
+  const [query, setQuery] = useState("");
+
+  return (
+    <>
+      <Container>
+        <div className='search'>
+          <h2> Local Restaurants</h2>
+          <InputGroup>
+            <InputGroupAddon addonType='append'> Search </InputGroupAddon>
+            <Input
+              onChange={(e) => setQuery(e.target.value.toLocaleLowerCase())}
+              value={query}
+            />
+          </InputGroup>
+          <br></br>
+        </div>
+      </Container>
+      <Row>
+        <Col
+          className='bg-dark border'
+          md='8'
+          lg='8'
+          xl='8'>
+          <Container>
+            <RestaurantList search={query} />
+          </Container>
+        </Col>
+        <Col
+          className='bg-light border'
+          md='4'
+          lg='4'
+          xl='4'>
+          {addItem.cart.items.length !== 0 && (
+            <Row xs='8'>
+              <Cart />
+            </Row>
+          )}
+        </Col>
+      </Row>
+    </>
+  );
+}
+export default Home;
